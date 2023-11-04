74b529f9588186414df108d27a742184de2b8a20
fdicera <67300276+fdicera@users.noreply.github.com>
2023-11-04 17:53:37 +0000

    Working on youngest page.

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -9,4 +9,18 @@ class DirectorsController < ApplicationController
     @the_director = matching_records.at(0)
     render({ :template => "director_templates/details"})
   end
+
+  def young
+    @young_dob = Director.maximum(:dob)
+    @young_person = Director.where({:dob => @young_dob})
+    render({ :template => "director_templates/young_details"})
+  end
+
+  def old
+    the_id = params.fetch("the_id")
+    matching_records = Director.where({ :id => the_id})
+    @the_director = matching_records.at(0)
+    render({ :template => "director_templates/old_details"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  # add youngest director page
+  get("/directors/youngest", { :controller => "directors", :action => "young" })
+
+   # add oldest director page
+   get("/directors/oldest", { :controller => "directors", :action => "old" })
+
   # name of table making
   get("/directors", { :controller => "directors", :action => "index" })
   # show = action that shows details of one record, convention
@@ -7,4 +14,9 @@ Rails.application.routes.draw do
   # add movies pages
   get("/movies/", { :controller => "movies", :action => "index" })
   get("/movies/:the_id", { :controller => "movies", :action => "show" })
+  # add actors pages
+  get("/actors/", { :controller => "actors", :action => "index" })
+  get("/actors/:the_id", { :controller => "actors", :action => "show" })
+
+
 end
