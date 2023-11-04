b73e356eafbef840d8594b4b77d73a8c3c882d27
fdicera <67300276+fdicera@users.noreply.github.com>
2023-11-04 15:39:06 +0000

    Adds directors#index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  # name of table making
+  get("/directors", { :controller => "directors", :action => "index" })
 end
