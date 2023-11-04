cda618ca9123d79cce680ba5f45ad85072bc680b
fdicera <67300276+fdicera@users.noreply.github.com>
2023-11-04 16:29:40 +0000

    Created directors page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   # name of table making
   get("/directors", { :controller => "directors", :action => "index" })
+  # show = action that shows details of one record, convention
+  get("/directors/:the_id", { :controller => "directors", :action => "show" })
 end
