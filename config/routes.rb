ed4b38b5689ccb666b7c47f17d3721598eaf8f19
fdicera <67300276+fdicera@users.noreply.github.com>
2023-11-04 16:55:12 +0000

    Added movies.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get("/directors", { :controller => "directors", :action => "index" })
   # show = action that shows details of one record, convention
   get("/directors/:the_id", { :controller => "directors", :action => "show" })
+  # add movies pages
+  get("/movies/", { :controller => "movies", :action => "index" })
+  get("/movies/:the_id", { :controller => "movies", :action => "show" })
 end
